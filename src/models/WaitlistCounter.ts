2271d41312e2a75fbfc4992b86a0f5b28e78066a
Dris Elamri <elamri@dris.llc>
2024-04-12 00:29:51 -0500

    deleted files

--- src/models/WaitlistCounter.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const waitlistCounterSchema = new mongoose.Schema({
-  count: { type: Number, required: true, default: 0 },
-});
-
-const WaitlistCounter = mongoose.model("WaitlistCounter", waitlistCounterSchema);
-
-export default WaitlistCounter;
