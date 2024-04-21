59a79419d370cde82dfb8d23c0b589175fbb7eef
Raine Soriano <raine.a.soriano@gmail.com>
2024-04-20 23:37:14 -0700

    Add mandatory fields to sign-in route

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,5 +1,6 @@
 import { getUser } from "@/app/utils";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 /* 
 Protected route
@@ -11,11 +12,36 @@ Protected route
 - phone number
 
 */
+
+const schema = z.object({
+    token: z.string({
+        required_error: "token is required"
+    }),
+    birthday: z.string({
+        required_error: "birthday is required"
+    }).datetime({
+        message: "birthday must be UTC"
+    }),
+    email: z.string({
+        required_error: "email is required"
+    }),
+    name: z.string({
+        required_error: "name is required"
+    }),
+    phoneNumber: z.string({
+        required_error: "phoneNumber is required"
+    })
+});
+
 export const POST = async (
     req: NextRequest
 ) => {
-    const data = await req.json();
-    const payload = await getUser(data);
+    const parse = await schema.safeParseAsync(await req.json());
+    console.log(parse);
+    if (!parse.success) {
+        return NextResponse.json({ error: parse.error.issues.map((issue) => issue.message)}, { status: 400 });
+    }
+    const payload = await getUser(parse.data);
     if (!payload) return NextResponse.json({ error: "Bad JWT" }, { status: 403 })
 
     
