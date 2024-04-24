d95b66321b09f48871f0de3e1f4fbe79db0c1324
Raine Soriano <rainearcher@g.ucla.edu>
2024-04-24 03:41:36 -0700

    Fix listing route GET

--- src/app/api/listings/route.ts
+++ src/app/api/listings/route.ts
@@ -25,8 +25,6 @@ export const GET = async (
   const { userId, payload } = await getUser(req);
   if (!payload) return NextResponse.json({ error: "Bad JWT" }, { status: 403 });
   if (!userId) return NextResponse.json({error: "clerkId not found"}, {status: 400});
-  let searchParams = req.nextUrl.searchParams;
-  searchParams.set("sellerId", userId);
-  return PrismaGET(searchParams, ListingModel.partial(), prisma.listing);
+  return PrismaGET(req.nextUrl.searchParams, ListingModel.partial(), prisma.listing);
 }
 
