4b49c13b2a7df3b4801d326e58e3568704ee5e77
Raine Soriano <rainearcher@g.ucla.edu>
2024-04-26 11:02:32 -0700

    Add rating and reviews to GET listing

--- src/app/api/listings/route.ts
+++ src/app/api/listings/route.ts
@@ -4,6 +4,7 @@ import { PrismaGET, PrismaPOST, getUser } from "@/app/utils";
 import { now } from "mongoose";
 import { z } from "zod";
 import { ListingModel } from "@zod-prisma";
+import { searchParamsToJSON, tryOrReturnError } from "@/app/utils";
 
 
 const prisma = new PrismaClient();
@@ -22,9 +23,43 @@ export const POST = async (
 export const GET = async (
     req: NextRequest
 ) => {
+return tryOrReturnError(async () => {
   const { userId, payload } = await getUser(req);
   if (!payload) return NextResponse.json({ error: "Bad JWT" }, { status: 403 });
   if (!userId) return NextResponse.json({error: "clerkId not found"}, {status: 400});
-  return PrismaGET(req.nextUrl.searchParams, ListingModel.partial(), prisma.listing);
+  
+  const whereParams: any = ListingModel.partial().safeParse(searchParamsToJSON(req.nextUrl.searchParams));
+  if (!whereParams.success) {
+    return NextResponse.json({ error: "Invalid search params"}, {status:400});
+  }
+
+
+  const listings = await Promise.all(await prisma.$transaction(async (prisma) => {
+    const listings = await prisma.listing.findMany({ where: whereParams.data });
+    return await listings.map(async (listing) => {
+      const reviews = await prisma.review.aggregate({
+        _count: {
+          id: true
+        },
+        _avg: {
+          rating: true
+        },
+        where: {
+          listingId: listing.id
+        }
+      });
+      return {
+        ...listing,
+        rating: reviews._avg.rating,
+        reviews: reviews._count.id
+      }
+    });
+  }))
+  
+
+  return NextResponse.json({ data: listings });
+  
+})
+  
 }
 
