b98667f74ae4e3ed7a1bf13e341b5ce9c8f40860
Raine Soriano <rainearcher@g.ucla.edu>
2024-04-27 14:48:10 -0700

    Update bid zod type

--- zod-prisma/bid.ts
+++ zod-prisma/bid.ts
@@ -3,8 +3,10 @@ import * as z from "zod"
 export const BidModel = z.object({
   id: z.string(),
   amount: z.coerce.number(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+  starts: z.coerce.date(),
+  ends: z.coerce.date(),
   userId: z.string(),
   listingId: z.string(),
 })
