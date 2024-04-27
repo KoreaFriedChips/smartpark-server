1c02e7ded4cbef792bbf8e7597c1d837259bb96e
Raine Soriano <rainearcher@g.ucla.edu>
2024-04-27 15:04:02 -0700

    Check valid bid amount before creation

--- src/app/api/bids/route.ts
+++ src/app/api/bids/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client/edge.js";
-import { PrismaGET, PrismaPOST, getUser } from "@/app/utils";
+import { PrismaGET, PrismaPOST, getUser, searchParamsToJSON, tryOrReturnError } from "@/app/utils";
 import { z } from "zod";
 import { BidModel } from "@zod-prisma";
 
@@ -9,12 +9,34 @@ const prisma = new PrismaClient();
 export const POST = async (
     req: NextRequest
 ) => {
+return tryOrReturnError(async () => {
   const { userId, payload } = await getUser(req);
   if (!payload) return NextResponse.json({ error: "Bad JWT" }, { status: 403 });
   if (!userId) return NextResponse.json({error: "clerkId not found"}, {status: 400});
+  
   let data: any = await req.json();
+  let getReq = new NextRequest("/api/bids");
+  getReq.nextUrl.searchParams.set('listingId', data.listingId);
+  getReq.nextUrl.searchParams.set('starts', data.starts);
+  getReq.nextUrl.searchParams.set('ends', data.ends);
+
+  const otherBids = await GET(getReq);
+  if (otherBids && otherBids.status === 200) {
+    const otherData:any = await otherBids.json();
+    if (data.amount < otherData.data[0].amount) 
+      return NextResponse.json({error: "bid amount must be higher than current max"}, {status:400});
+  }
+
+  
   data.userId = userId;
-  return PrismaPOST(req, prisma.bid);
+  const createdObject = await prisma.bid.create({
+    data: data
+  });
+
+  return NextResponse.json({ data: createdObject });
+
+})
+
 }
 
 export const GET = async (
