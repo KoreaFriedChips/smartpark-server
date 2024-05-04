dd2f9a4602c3c6d9c7b685ff4c16059b05853543
Raine Soriano <rainearcher@g.ucla.edu>
2024-05-03 20:32:09 -0700

    Fix bid routes

--- src/app/api/bids/route.ts
+++ src/app/api/bids/route.ts
@@ -15,19 +15,19 @@ return tryOrReturnError(async () => {
   if (!userId) return NextResponse.json({error: "clerkId not found"}, {status: 400});
   
   let data: any = await req.json();
-  let getReq = new NextRequest("/api/bids");
-  getReq.nextUrl.searchParams.set('listingId', data.listingId);
-  getReq.nextUrl.searchParams.set('starts', data.starts);
-  getReq.nextUrl.searchParams.set('ends', data.ends);
 
-  const otherBids = await GET(getReq);
-  if (otherBids && otherBids.status === 200) {
-    const otherData:any = await otherBids.json();
-    if (data.amount < otherData.data[0].amount) 
-      return NextResponse.json({error: "bid amount must be higher than current max"}, {status:400});
-  }
+  const highestBid = await prisma.bid.findFirst({
+    where: {
+      listingId: data.listingId,
+      starts: data.starts,
+      ends: data.ends
+    },
+    orderBy: {
+      amount: 'desc'
+    }
+  });
+  if (highestBid && Number(data.amount) <= highestBid.amount) return NextResponse.json({error: "bid amount must be higher than current max"}, {status: 400})
 
-  
   data.userId = userId;
   const createdObject = await prisma.bid.create({
     data: data
@@ -50,36 +50,8 @@ return tryOrReturnError(async () => {
   let whereParams = BidModel.partial().safeParse(searchParamsToJSON(req.nextUrl.searchParams));
   if (!whereParams.success) return NextResponse.json({ error: "Invalid search params"}, {status:400});
 
-
-  let range: any = undefined;
-  if ('starts' in whereParams.data || 'ends' in whereParams.data) {
-    if (!('starts' in whereParams.data) || !('ends' in whereParams.data)) 
-      return NextResponse.json({error: "if searching range, must include both 'starts' and 'ends' params"}, {status: 400});
-    const start = whereParams.data.starts;
-    const end = whereParams.data.ends;
-    delete whereParams.data.starts;
-    delete whereParams.data.ends;
-    range = {
-     OR: [
-      {
-        starts: { gt: start, lt: end}
-      },
-      {
-        ends: { gt: start, lt: end}
-      }
-     ] 
-    }
-  }
-  console.log(whereParams);
-  console.log(range);
-
   const objects = await prisma.bid.findMany({ 
-    where: {
-      AND: [
-        whereParams.data,
-        range
-      ]
-    },
+    where: whereParams.data,
     orderBy: {
       amount: 'desc',
     }
