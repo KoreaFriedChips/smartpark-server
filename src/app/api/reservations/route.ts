4e0082131dad939e079e90bfcb358a665b16c735
Raine Soriano <rainearcher@g.ucla.edu>
2024-05-03 20:04:35 -0700

    Remove availability when reservation is created

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -2,6 +2,7 @@ import { PrismaPOST, tryOrReturnError } from "@/app/utils";
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getUser } from "@/app/utils";
+import { isEqual, interval } from "date-fns";
 
 export const POST = async (
   req: NextRequest,
@@ -12,5 +13,33 @@ return tryOrReturnError(async () => {
   if (!userId) return NextResponse.json({error: "clerkId not found"}, {status: 400});
   let data: any = await req.json();
   data.userId = userId;
-  return PrismaPOST(data, prisma.reservation);
+  if (!data.listingId) throw new Error("listingId required for reservation");
+  if (!data.starts || !data.ends) throw new Error("interval needed for reservation");
+
+
+  const reservation = await prisma.$transaction(async (tx) => {
+    const listing = await tx.listing.findUnique({
+      where: { id: data.listingId },
+      select: { availability: true }
+    });
+    if (!listing) throw new Error("listing not found");
+    
+    const reserved: Interval = interval(data.starts, data.ends)
+    const newAvailability = listing.availability.filter(({start, end}) => !isEqual(reserved.start, start) && !isEqual(reserved.end, end));
+    await tx.listing.update({ 
+      where: { 
+        id: data.listingId 
+      }, 
+      data: {
+        availability: newAvailability
+      },
+    })
+  
+    const createdObject = await tx.reservation.create({
+      data: data
+    });
+
+    return createdObject;
+  })
+  return NextResponse.json({data: reservation});
 })}
