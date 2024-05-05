0d058c6c344fc46e11ae266166b6ecd6ecb50ab3
Raine Soriano <rainearcher@g.ucla.edu>
2024-05-05 03:00:40 -0700

    Add reservation GET route

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -1,9 +1,10 @@
-import { PrismaPOST, tryOrReturnError } from "@/app/utils";
+import { PrismaGET, PrismaPOST, tryOrReturnError } from "@/app/utils";
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getUser } from "@/app/utils";
 import { isEqual, interval } from "date-fns";
 import { createReservation } from "@/lib/reservation";
+import { ReservationModel } from "@zod-prisma";
 
 export const POST = async (
   req: NextRequest,
@@ -20,4 +21,14 @@ return tryOrReturnError(async () => {
 
   const reservation = await createReservation(data);
   return NextResponse.json({data: reservation});
+})}
+
+export const GET = async (
+  req: NextRequest
+) => {
+return tryOrReturnError(async () => {
+  const { userId, payload } = await getUser(req);
+  if (!payload) return NextResponse.json({ error: "Bad JWT" }, { status: 403 });
+  if (!userId) return NextResponse.json({error: "clerkId not found"}, {status: 400});
+  return PrismaGET(req.nextUrl.searchParams, ReservationModel.partial(), prisma.reservation);
 })}
