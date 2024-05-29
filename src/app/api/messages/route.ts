2d6a2727cb867237d7293bd8515ff23678cf32e8
Raine Soriano <rainearcher@g.ucla.edu>
2024-05-28 22:41:45 -0700

    require second userid for message GET route

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -32,20 +32,27 @@ return tryOrReturnError(async () => {
   const { userId, payload } = await getUser(req);
   if (!payload) return NextResponse.json({ error: "Bad JWT" }, { status: 403 });
   if (!userId) return NextResponse.json({error: "clerkId not found"}, {status: 400});
+  const otherUserId = req.nextUrl.searchParams.get('userId');
+  if (!otherUserId) return NextResponse.json({error: "userId required"}, {status: 400});
 
   const messages = await prisma.message.findMany({
     where: {
       OR: [
         {
-          toUserId: userId
+          fromUserId: otherUserId,
+          toUserId: userId,
         },
         {
-          fromUserId: userId
+          fromUserId: userId,
+          toUserId: otherUserId
         }
       ]
+    },
+    orderBy: {
+      date: 'desc',
     }
   });
-
+  
   return NextResponse.json({data: messages});
 })
 }
